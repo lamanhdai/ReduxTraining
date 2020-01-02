3e717b4f4a417faf00d4a6cf99372931a37c7340
anh-dai.lam <anh-dai.lam@ekino.com>
2020-01-02 14:08:02 +0700

    react redux

--- user-card-app/src/components/App.js
+++ user-card-app/src/components/App.js
@@ -1,18 +1,38 @@
 import React, { useState } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+// import { ReactReduxContext } from 'react-redux';
 import './App.css';
 import { Introduction } from './Introduction';
 import { UserDetail } from './UserDetail';
-import { changeJob } from  '../actions';
-import { store } from '../store';
+import { changeJob } from '../actions';
+// import { store } from '../store';
+
+// function useSelector(selector) {
+//   const { store } = useContext(ReactReduxContext);
+//   const [result, setResult] = useState(selector(store.getState()));
+//   useEffect(() => {
+//     const unsubscribe = store.subscribe(() => {
+//       const newResult = selector(store.getState());
+//       if (newResult !== result) {
+//         setResult(newResult);
+//       }
+//     })
+//     return unsubscribe;
+//   }, []);
+//   return result;
+// }
+
+// function useDispatch() {
+//   const { store } = useContext(ReactReduxContext);
+//   return store.dispatch;
+// }
 
 function App() {
-  const [newJob, setNewJob] = useState('');
-  const { url, text } = useSelector(state => state.photo);
-  const { job, hobby } = useSelector(state => state.introduction);
-  const { name, location } = useSelector(state => state.detail);
-  const dispatch = useDispatch();
-   
+    const [newJob, setNewJob] = useState('');
+    const { url, text } = useSelector(state => state.photo);
+    const { job, hobby } = useSelector(state => state.introduction);
+    const { name, location } = useSelector(state => state.detail);
+    const dispatch = useDispatch();
     return (
         <div className="App">
             <div className="Card">
@@ -28,7 +48,13 @@ function App() {
                 </div>
             </div>
             <input type="text" onChange={e => setNewJob(e.target.value)} value={newJob} />
-            <button onClick={() => dispatch(changeJob(newJob))}>Change Job</button>
+            <button
+                onClick={() => {
+                    dispatch(changeJob(newJob))
+                }}
+            >
+                Change Job
+            </button>
         </div>
     );
 }
